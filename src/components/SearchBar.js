c66b0ec22493e1ae75bc5105825844a982b25fc2
Feixuan Li <feixuanl@usc.edu>
2019-01-21 16:12:18 +0800

    form handling

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -5,10 +5,16 @@ class SearchBar extends React.Component {
         term: '',
     }
 
+    onFormSubmit(event) {
+        event.preventDefault();
+        this.props.onSubmit(this.state.term);
+    }
+
+
     render() {
         return (
             <div className="ui segment">
-                <form className="ui form">
+                <form className="ui form" onSubmit={(event) => this.onFormSubmit(event)}>
                     <div className="field">
                         <label>Image Search</label>
                         <input 
